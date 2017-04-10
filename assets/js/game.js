65ea60851b155ae3f1a08415f6e0126c12ff8b6f
amir <info@amirizad.com>
2017-04-09 23:16:24 -0700

    some corrections and improvements

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,23 +5,27 @@ $(window).on('load', function () {
 	
 	createObjects();
 
-	$('#hangmanintro').click(function(){
+	//Startup image click listener
+	$('#startup').click(function(){
 		$('#wordpanel').removeClass('hide');
+		$('#hangman').removeClass('hide');
 	});
 	
 	//change the category
 	$('#category').change(function() {
-		updateCat();	
+		changeCategory();	
+		updateObject(curObjName);
 		loadObject(curObjName);
 	});
 
-	//Click the start button listener
+	//Click the Restart button listener
 	$('#restartbtn').click(function(event){
 		setObject(curObjName);
 		loadObject(curObjName);
 		event.preventDefault();		
 	});
 
+	//Click the Next button listener	
 	$('#nextbtn').click(function(event){
 		updateObject(curObjName);
 		event.preventDefault();		
@@ -30,33 +34,23 @@ $(window).on('load', function () {
 	
 	//keybord key listener
 	$(document).on('keyup', function(event){
-		var curLetter = event.key.toLowerCase();
+		var legKey = true;
+		var letter = event.key.toLowerCase();
 		var obj = HangMan[curObjName];
 		if ( event.which < 65 || event.which > 90) {
 			legKey = false;
 		}else{
 			for( i = 0 ; i < obj.guesses.length ; i++){
-				if(curLetter === obj.guesses[i]){legKey = false};
+				if(letter === obj.guesses[i]){
+					legKey = false;
+					break;
+				};
 			};
 		};
 		if (legKey) {
-			var obj = HangMan[curObjName];
-			var curLetter = event.key.toLowerCase();
-			obj.guesses.push(curLetter);
-			var guessStatus = updateGuesses(obj.word , curLetter , obj.falseg.length);
-			if(guessStatus){
-				obj.trueg.push(curLetter);
-			}else{
-				obj.falseg.push(curLetter);
-			};
-			var status = updateStatus(obj.word, obj.trueg.length , obj.falseg.length);
-			if (status === 1){
-				obj.wins++;
-				$('#wins').html($('#wins').html()+1)
-			}else if (status === 0){
-				obj.loses++;
-				$('#loses').html($('#wins').html()+1)
-			};
+			processLetter(event.key.toLowerCase());
+		}else{
+			event.preventDefault();
 		}; 
 	});		
 });
@@ -65,7 +59,6 @@ var namesArr = ['ava', 'Rey', 'Jona', 'John', 'Edgar', 'Brayan', 'Leonel', 'Garn
 var colorsArr = ['red', 'blue', 'aqua', 'lime', 'black', 'white', 'maroon', 'orange', 'purple', 'fuchsia'];
 var statesArr = ['ohio', 'iowa', 'idaho', 'texas', 'alaska', 'oregon', 'vermont', 'wyoming', 'delaware', 'maryland'];
 var categories = ['namesObj', 'colorsObj','statesObj'];
-var legKey = true;
 var gameObj = {
 	words : [],
 	category : "",
@@ -78,10 +71,11 @@ var gameObj = {
 	falseg : []
 };
 
-function updateCat(){
+//Change Game to new category
+function changeCategory(){
 	curObjName = $('#category').val();
 	$('#curobj').val(curObjName);		
-}
+};
 
 //Creating objects for all categories
 function createObjects(){
@@ -96,10 +90,11 @@ function createObjects(){
 	setObject('statesObj');
 	var catIndex = Math.floor(Math.random() * categories.length);
 	$('#category option:eq(' + catIndex + ')').prop('selected', true)
-	updateCat();
+	changeCategory();
 	loadObject(categories[catIndex]);
 };
 
+//Set a clear Hangman object
 function setObject(objectName){
 	var obj = HangMan[objectName];
 	obj.category = $('#category option[value=' + objectName + ']').text();
@@ -109,6 +104,7 @@ function setObject(objectName){
 	obj.lives = 10;
 	obj.guesses = [];
 	obj.trueg = [];
+	for ( i = 0 ; i < obj.word.length ; i++ ){obj.trueg.push('0')};
 	obj.falseg = [];
 };
 
@@ -119,6 +115,7 @@ function updateObject(objectName){
 	obj.lives = 10;
 	obj.guesses = [];
 	obj.trueg = [];
+	for ( i = 0 ; i < obj.word.length ; i++ ){obj.trueg.push('0')};
 	obj.falseg = [];
 	loadObject(objectName);
 };
@@ -138,6 +135,12 @@ function loadObject(objectName){
 	$('.hang').addClass('hide');
 };
 
+function charOccur(string,char) {
+	var re = new RegExp(char,"gi");
+	re = string.match(re) === null ? "" : string.match(re);
+	return re.length;
+};
+
 //Loading the word into the word panel
 function loadWord(word){
 	var span = "";
@@ -147,39 +150,57 @@ function loadWord(word){
 	return span;
 };
 
+//Return the indexes of a letter in a word
+function findIndex(letter,word){
+	var indexArray = [];
+	for(var i = 0; i < word.length; i++){
+		if ( letter === word[i]){
+			indexArray.push(i);
+		}
+	}
+	return indexArray;
+};
 
-function updateGuesses(word,letter,falseg){
- 	var index = word.indexOf(letter);
- 	legKey = true;
- 	if (index < 0){
+//Processing the input letter
+function processLetter(letter){
+	var obj = HangMan[curObjName];
+	var word = obj.word;
+	var guesses = obj.guesses;
+	var falseg = obj.falseg;
+	var trueg = obj.trueg;
+	guesses.push(letter);
+ 	var letterIndex = findIndex(letter,word);
+ 	if (letterIndex.length === 0){
+ 		falseg.push(letter);
  		var font = '<font>'+ letter +'</font>';
  		$('#guesses').append(font);
  		if ( falseg < 9 ) {$('#hangman' + falseg).removeClass('hide')};
- 		$('#live' + falseg).addClass('off');
+ 		var remLive = parseInt($('#remlive').html()) - 1;
+ 		$('#remlive').html(remLive);
+ 		$('#live' + falseg.length).addClass('off');
  		var letterfailed = document.getElementById("letterfailed");
- 		letterfailed.play();
- 		return false;
+ 		if( remLive > 0 ){ letterfailed.play() };
  	}else{
-		$('#word span:nth-child(' + (index + 1) + ')').html(letter);
-		var lettersucceeded = document.getElementById("lettersucceeded");
+ 		for( i = 0 ; i < letterIndex.length ; i++){
+ 			var j = letterIndex[i];
+ 			trueg[j] = "1";
+ 			$('#word span:nth-child(' + (j + 1) + ')').html(letter);
+ 		}
+ 		var lettersucceeded = document.getElementById("lettersucceeded");
 		lettersucceeded.play();
-		return true;
- 	}
-};
-
-function updateStatus(word, trueg, falseg){
-	if (word.length === trueg){
-		$('#hangmanw').css('display','block');
-		$('#showword').html(word);
+ 	};
+	if (charOccur(trueg.join(''),'0') === 0){
 		var wordsucceeded = document.getElementById("wordsucceeded");
 		wordsucceeded.play();
-		return 1;
-	}else if (word + 10 <= trueg + falseg){
-		$('#hangmanl').css('display','block');
+		$('#hangmanw').removeClass('hide');
+		$('#showword').html(word);
+		obj.wins++;
+		$('#wins').html(parseInt($('#wins').html()) + 1);		
+	}else if (falseg.length === 10){
+		$('#hangmanl').removeClass('hide');
 		var wordfailed = document.getElementById("wordfailed");
 		wordfailed.play();
-		return 0;
-	}else{
-		return -1;
-	}
+		obj.loses++;
+		$('#loses').html(parseInt($('#loses').html()) + 1);
+	};
 };
